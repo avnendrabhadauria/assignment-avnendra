7b6226c573403745f6958c728904c62d5f7a1d62
Avnendnra <avnendra.bhadauria@gmail.com>
2023-09-16 19:02:21 +0530

    createElement

--- src/slot/Slot.js
+++ src/slot/Slot.js
@@ -1,5 +1,6 @@
 import "./slot.scss";
 
+import { ReactComponent as Cancel } from "../assests/cancel.svg";
 import React from "react";
 import Tasks from "../task/Tasks";
 
@@ -16,10 +17,13 @@ function Slot(props) {
     <div className="slot-placeholder">
       <div className="slot">
         <header className="slot-header">
-          <div>
+          <div className="slot-title">
             <label style={style}>
               <strong>SLOT:</strong> {data?.slot}
             </label>
+          </div>
+          <div className="slot-cancel">
+            <Cancel />
           </div>
         </header>
         <Tasks tasklist={data?.task} />
